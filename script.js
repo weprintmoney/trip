ce2b76d5c42f66c6fe3f4a95deb3c20ddef8cac0
Charlcye Mitchell <131167825+weprintmoney@users.noreply.github.com>
2026-06-28 14:39:03 -0500

    Remove Eiffel Tower from Paris stop

--- script.js
+++ script.js
@@ -56,12 +56,6 @@ const TRIP = {
           tag: "activity",
         },
         {
-          time: "9:15 PM (Thu Jul 9)",
-          name: "Eiffel Tower",
-          detail: "Sunset + first sparkle show at 10:00 PM",
-          tag: "activity",
-        },
-        {
           time: "10:00 PM (Thu Jul 9)",
           name: "Dinner at Girafe",
           detail: "2 people · Reservation confirmed",
