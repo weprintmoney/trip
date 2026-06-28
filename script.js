0adf1e4ee5ee74f4e72701ef726f72a773709fbf
Charlcye  Mitchell <cmitchell@cyborg.co>
2026-06-28 18:42:41 -0500

    Add Airbnb links to massage, Madeira tour, catamaran, and photo shoot

--- script.js
+++ script.js
@@ -80,6 +80,7 @@ const TRIP = {
           time: "7:00–8:15 PM (Thu Jul 9)",
           name: "Couple's Photo Session",
           detail: "Hosted by Jon · +11 others · 1.25 hours",
+          link: "https://www.airbnb.com/experiences/252431",
           tag: "activity",
         },
         {
@@ -113,12 +114,13 @@ const TRIP = {
       departure: [{ date: "Jul 16", num: "BA 2717", route: "Funchal → London",   time: "12:20 PM WEST" }],
       events: [
         { time: "3:00 PM (Thu Jul 10)",      name: "Check-in",             tag: "lodging" },
-        { time: "6:00–7:00 PM (Thu Jul 10)", name: "Massage at the house", photo: "images/madeira-massage.avif", tag: "activity" },
+        { time: "6:00–7:00 PM (Thu Jul 10)", name: "Massage at the house", photo: "images/madeira-massage.avif", link: "https://www.airbnb.com/services/6961430", tag: "activity" },
         {
           time: "6:00 AM–1:00 PM (Tue Jul 14)",
           name: "Sunrise Tour of Madeira",
           detail: "Sunrise in secret spots off the tourist path · Hiking mountain peaks · Chasing waterfalls through ancient forest · Walking century-old village paths",
           photo: "images/madeira-tour.avif",
+          link: "https://www.airbnb.com/experiences/3911233",
           tag: "activity",
         },
         {
@@ -126,6 +128,7 @@ const TRIP = {
           name: "Luxury Catamaran & Dolphins – All Inclusive",
           detail: "Hosted by Maria & Jerome · +22 others · 3 hours on the water",
           photo: "images/madeira-catamaran.avif",
+          link: "https://www.airbnb.com/experiences/780563",
           tag: "activity",
         },
       ],
